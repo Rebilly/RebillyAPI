25b4aed77604d410b79e950424590c52d4574482
Bogdan L <thaddeusbox@gmail.com>
2017-01-05 01:37:35 +0600

    Add examples for gateway accounts resource

---
+++ spec/code_samples/PHP/gateway-accounts@{id}/put.php
@@ -0,0 +1,24 @@
+$gatewayAccountForm = new Rebilly\Entities\GatewayAccount();
+$paymentCardScheme = new Rebilly\Entities\PaymentCardScheme();
+$paymentMethod = new Rebilly\Entities\PaymentMethod();
+
+$gatewayAccountForm->setGatewayName('A1Gateway');
+$gatewayAccountForm->setAcquirerName('Bank of Rebilly');
+$gatewayAccountForm->setOrganizationId('organizationId');
+$gatewayAccountForm->setMerchantCategoryCode(5734);
+$gatewayAccountForm->setWebsites(['websiteId1', 'websiteId2']);
+$gatewayAccountForm->setPaymentCardSchemes([$paymentCardScheme::SCHEME_VISA, $paymentCardScheme::SCHEME_MASTERCARD]);
+$gatewayAccountForm->setMethod($paymentMethod::METHOD_CASH);
+
+$gatewayConfig = [
+'accountId' => 'test',
+'password' => '123',
+];
+
+$gatewayAccountForm->setGatewayConfig($gatewayConfig);
+
+try {
+    $gatewayAccount = $client->gatewayAccounts()->update('gatewayAccountId', $gatewayAccountForm);
+} catch (UnprocessableEntityException $e) {
+    echo $e->getMessage();
+}
