0f423389909ede33f63a1b3fe53346f414839820
Bogdan L <thaddeusbox@gmail.com>
2017-01-05 14:34:54 +0600

    Fix PUT method

--- spec/code_samples/PHP/gateway-accounts@{id}/put.php
+++ spec/code_samples/PHP/gateway-accounts@{id}/put.php
@@ -1,14 +1,18 @@
 $gatewayAccountForm = new Rebilly\Entities\GatewayAccount();
-$paymentCardScheme = new Rebilly\Entities\PaymentCardScheme();
-$paymentMethod = new Rebilly\Entities\PaymentMethod();
 
 $gatewayAccountForm->setGatewayName('A1Gateway');
 $gatewayAccountForm->setAcquirerName('Bank of Rebilly');
 $gatewayAccountForm->setOrganizationId('organizationId');
 $gatewayAccountForm->setMerchantCategoryCode(5734);
-$gatewayAccountForm->setWebsites(['websiteId1', 'websiteId2']);
-$gatewayAccountForm->setPaymentCardSchemes([$paymentCardScheme::SCHEME_VISA, $paymentCardScheme::SCHEME_MASTERCARD]);
-$gatewayAccountForm->setMethod($paymentMethod::METHOD_CASH);
+$gatewayAccountForm->setWebsites([
+    'websiteId1',
+    'websiteId2',
+]);
+$gatewayAccountForm->setPaymentCardSchemes([
+    Rebilly\Entities\PaymentCardScheme::SCHEME_VISA,
+    Rebilly\Entities\PaymentCardScheme::SCHEME_MASTERCARD,
+]);
+$gatewayAccountForm->setMethod(Rebilly\Entities\PaymentMethod::METHOD_CASH);
 
 $gatewayConfig = [
     'accountId' => 'test',
