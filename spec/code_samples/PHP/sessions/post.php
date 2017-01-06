0d01510b431903b6b6e6c5b06942cf2612eb1593
Bogdan L <thaddeusbox@gmail.com>
2017-01-06 13:09:34 +0600

    Change example permissions

--- spec/code_samples/PHP/sessions/post.php
+++ spec/code_samples/PHP/sessions/post.php
@@ -1,6 +1,7 @@
 $sessionForm = new Rebilly\Entities\Session();
 
 $permissions = [
+    // Example permission to use GET and POST methods only for certain Customers
     [
         'resourceName' => Rebilly\Entities\ResourceType::TYPE_CUSTOMERS,
         'methods' => [
@@ -12,17 +13,19 @@ $permissions = [
             'testCustomerId2',
         ],
     ],
+    // Example permission to use all methods for Websites resource
     [
         'resourceName' => Rebilly\Entities\ResourceType::TYPE_WEBSITES,
         'methods' => [
             $sessionForm::METHOD_GET,
             $sessionForm::METHOD_POST,
-        ],
-        'resourceIds' => [
-            'testWebsiteId',
-            'testWebsiteId2',
+            $sessionForm::METHOD_PUT,
+            $sessionForm::METHOD_HEAD,
+            $sessionForm::METHOD_DELETE,
         ],
     ],
+    // Example permission to use all methods for all resources
+    [],
 ];
 
 $sessionForm->setPermissions($permissions);
