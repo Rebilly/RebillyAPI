6e73fef0a0e6e7b598a8fd5914651b8699221bd2
Demi <2e3s19@gmail.com>
2018-06-05 16:32:52 -0400

    Return old SDK example

--- spec/code_samples/PHP/cancellations/post.php
+++ spec/code_samples/PHP/cancellations/post.php
@@ -1,9 +1,8 @@
 $subscriptionCancelForm = new Rebilly\Entities\SubscriptionCancel();
 $subscriptionCancelForm->setPolicy($subscriptionCancelForm::AT_NEXT_RENEWAL);
-$subscriptionCancelForm->setSubscriptionId('subscriptionId');
 
 try {
-    $subscription = $client->subscriptions()->cancel($subscriptionCancelForm);
+    $subscription = $client->subscriptions()->cancel('subscriptionId', $subscriptionCancelForm);
 } catch (UnprocessableEntityException $e) {
     echo $e->getMessage();
 }
